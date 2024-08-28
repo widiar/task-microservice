7eb4b174abf6ff934be2e8772a2837481ab9e974
Ari Widiarsana <awidiarsana.aw@gmail.com>
2024-08-28 21:20:23 +0700

    changes name folder

--- Day 3/microservicesRest/src/main/java/id/co/bca/intra/traning/microservicesRest/entity/UserDto.java
+++
@@ -1,9 +0,0 @@
-package id.co.bca.intra.traning.microservicesRest.entity;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    private String userId;
-    private String nama;
-}
--- Day 3/microservicesRest/src/main/java/id/co/bca/intra/traning/microservicesRest/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package id.co.bca.intra.traning.microservicesRest.repository;
-
-import id.co.bca.intra.traning.microservicesRest.entity.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
